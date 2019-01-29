e9f6f41a3c75fd21b8eacfa9f9b037ee53aed226
Adán <adanjogohe@gmail.com>
2019-01-29 04:57:13 +0000

    [QOL]Quality of life
    + Moved The animation Script to a new js file
    + Started to move all Style tags to a css file
    + A lot of Quality of life made to the code.

---
+++ js/timing.js
@@ -0,0 +1,38 @@
+setTimeout(function () 
+			{$('.zeroeth').show().addClass('animated fadeIn');}, 1000);
+
+		setTimeout(function () 
+			{$('.first').show().addClass('animated fadeIn');}, 1500);
+
+		setTimeout(function () 
+			{$('.second').show().addClass('animated fadeInRight');}, 1000);
+
+		setTimeout(function () 
+			{$('.third').show().addClass('animated fadeInRight');}, 1050);
+
+		setTimeout(function () 
+			{$('.fourth').show().addClass('animated fadeInRight');}, 1100);
+
+		setTimeout(function ()
+			{$('.fifth').show().addClass('animated fadeInRight');}, 1150);	
+
+		setTimeout(function ()
+			{$('.sixth').show().addClass('animated fadeInRight');}, 1200);
+
+		setTimeout(function () 
+			{$('.seventh').show().addClass('animated fadeInRight');}, 1250);
+
+		setTimeout(function () 
+			{$('.eigth').show().addClass('animated fadeInRight');}, 1300);
+
+		setTimeout(function () 
+			{$('.ninth').show().addClass('animated fadeInRight');}, 1350);
+
+		setTimeout(function () 
+			{$('.tenth').show().addClass('animated fadeInRight');}, 1400);
+
+		setTimeout(function () 
+			{$('.eleventh').show().addClass('animated fadeInRight');}, 1450);
+
+		setTimeout(function () 
+			{$('.twelth').show().addClass('animated fadeInRight');}, 1500);
